a49f46fe82a46d1a18debd307a989e0ff4e33072
Sahil <sahilgghh@gmail.com>
2024-08-31 19:52:01 +0530

    backend url change

--- src/utils/info.js
+++ src/utils/info.js
@@ -1,3 +1,2 @@
-const BASE_URL = "https://notes-app-lubt.onrender.com"
-//const BASE_URL = "http://localhost:8080"
+const BASE_URL = "https://notes-app-production-7f75.up.railway.app"
 export {BASE_URL}
